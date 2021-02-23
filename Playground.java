beb283afb6b15ad5b4ab92ce278a878d0a435221
Pharoah Jardin <pharoah8060@gmail.com>
2021-02-23 03:15:08 +0100

    Added some powerup types, updated names

--- Playground.java
+++ Playground.java
@@ -6,29 +6,36 @@ import java.awt.event.*;
 
 
 public class Playground extends JPanel {
-    private transient ArrayList<Sprite> sprites = new ArrayList<>();
-    private static final int BALL_RADIUS = 10;
     private static Random rand = new Random();
 
-    private Paddle playerPaddle = new Paddle(new Vector(0, 375));
+    private transient ArrayList<Ball> balls = new ArrayList<>();
+    private transient ArrayList<Brick> bricks = new ArrayList<>();
+    private transient Paddle playerPaddle = new Paddle(new Vector(0, 375));
 
-    public Playground(int n) {
+    class PowerupType {
+        static final int NONE = 0;
+        static final int EXTRA_BALL = 1;
+        static final int FREEZE_BRICKS = 2; // bricks stop coming down
+        static final int BLAZING_BALL = 3; // makes balls go through bricks without colliding?
+    }
+
+    public Playground() {
         super();
-        for (int i=0; i<n; i++) {
-            Vector pos = new Vector(
-                rand.nextDouble() * 400,
-                rand.nextDouble() * 400
-            );
-            Vector vel = new Vector(
-                rand.nextDouble() * 2,
-                rand.nextDouble() * 2 
-            );
-            sprites.add(new Sprite(pos, vel));
-        }
+
+        Vector pos = new Vector(
+            200,
+            365
+        );
+        Vector vel = new Vector(
+            (rand.nextDouble() - 0.5) * 4,
+            - rand.nextDouble() * 4 
+        );
+        balls.add(new Ball(pos, vel));
 
         this.addMouseMotionListener(new MouseMotionListener() {
             public void mouseMoved(MouseEvent e) {
-                playerPaddle.setPos(new Vector(e.getX(), 375));
+                // playerPaddle.setPos(new Vector(e.getX(), 375));
+                playerPaddle.getPos().setX(e.getX());
             }
             
             public void mouseDragged(MouseEvent e) {
@@ -54,20 +61,11 @@ public class Playground extends JPanel {
         //clear panel
         g.clearRect(0, 0, 400, 400);
 
-        for (Sprite s : sprites) {
-            Vector pos = s.getPos();
-            g.setColor(Color.red);
-            g.fillOval(
-                (int) pos.getX() - BALL_RADIUS, 
-                (int) pos.getY() - BALL_RADIUS, 
-                2 * BALL_RADIUS,
-                2 * BALL_RADIUS
-            );
+        for (Ball b : balls) {
+            b.draw(g);
         }
 
-        Vector ppPos = playerPaddle.getPos();
-        g.setColor(Color.BLACK);
-        g.fillRect((int)ppPos.getX(), (int)ppPos.getY(), 50, 10);
+        playerPaddle.draw(g);
 
         this.repaint();
     }
@@ -76,39 +74,36 @@ public class Playground extends JPanel {
      * Moves all sprites according to their speed.
      */
     public void animate() {
-        for (Sprite s : sprites) {
-            s.update();
+        for (Ball b : balls) {
+            b.update();
         }
     }
         
     /**
-     * Checks for collisions with walls and bounces sprites.
+     * Checks for collisions with walls and bounces balls.
      */
     public void doPhysics() {
-        for (Sprite s : sprites) {
-            Vector sPos = s.getPos();
-            Vector sVel = s.getVel();
+        for (Ball b : balls) {
+            Vector pos = b.getPos();
+            Vector vel = b.getVel();
+            double r = b.getRadius();
 
-            if (sPos.getX() < BALL_RADIUS) {
-                sPos.setX(BALL_RADIUS);
-                sVel.scaleX(-1);
-            } else if (sPos.getX() > 400 - BALL_RADIUS) {
-                sPos.setX(400.0 - BALL_RADIUS);
-                sVel.scaleX(-1);
+            if (pos.getX() < r) {
+                pos.setX(r);
+                vel.scaleX(-1);
+            } else if (pos.getX() > 400 - r) {
+                pos.setX(400.0 - r);
+                vel.scaleX(-1);
             }
 
-            if (sPos.getY() < BALL_RADIUS) {
-                sPos.setY(BALL_RADIUS);
-                sVel.scaleY(-1);
-            } else if (sPos.getY() > 400 - BALL_RADIUS) {
-                sPos.setY(400.0 - BALL_RADIUS);
-                sVel.scaleY(-1);
+            if (pos.getY() < r) {
+                pos.setY(r);
+                vel.scaleY(-1);
             }
+            // } else if (pos.getY() > 400 - r) {
+            //     pos.setY(400.0 - r);
+            //     vel.scaleY(-1);
+            // }
         }
-    }
-
-    public static void main(String[] args) {
-        Playground p = new Playground(10);
-        System.out.println(p.sprites);
     }
 }
